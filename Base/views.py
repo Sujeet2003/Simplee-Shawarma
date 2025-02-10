5152722e2fb8b201a32cc88138563756ff8c18ec
Sujeet2003 <sujeetsmpatel@gmail.com>
2025-02-10 18:57:35 +0530

    working with footer

--- Base/views.py
+++ Base/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "home.html")
+    context = {
+        
+    }
+    return render(request, "home.html", context)
